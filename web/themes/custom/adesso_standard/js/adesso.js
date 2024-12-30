76c84c9543bdacde278963ce0feb34c3894d8592
Igor Besel <igor.besel@adesso.de>
2024-12-30 16:54:44 +0100

    Füge das adesso_standard-Theme hinzu und aktualisiere die CI/CD-Konfiguration

--- web/themes/custom/adesso_standard/js/adesso.js
+++ web/themes/custom/adesso_standard/js/adesso.js
@@ -1,19 +1,1 @@
 import 'flowbite/dist/flowbite.js';
-
-/**
- * @file
- * adesso_standard behaviors.
- */
-(function (Drupal) {
-
-  'use strict';
-
-  Drupal.behaviors.adessoStandard = {
-    attach (context, settings) {
-
-      console.log('It works!');
-
-    }
-  };
-
-} (Drupal));
